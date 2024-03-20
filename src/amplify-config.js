bd017e733b1b5990d3d3cf2181dc4f91587fe629
JuntaLaw <junta_law@me.com>
2024-03-20 17:50:13 +0900

    tailwind css, Daisy UI Add

--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,4 +1,3 @@
-// 以下の行を追加
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
